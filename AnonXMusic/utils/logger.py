9d158c9d17f37e72afb6be23c48ecdbf9dd0100c
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-06 21:59:57 +0700

    Update logger.py

--- AnonXMusic/utils/logger.py
+++ AnonXMusic/utils/logger.py
@@ -8,7 +8,7 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<blockquote>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
 <b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
 <b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
 <b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
 
 <b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}</blockquote>"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
