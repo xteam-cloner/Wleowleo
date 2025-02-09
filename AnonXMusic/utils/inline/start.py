de0268f2b4ba3079c79ae4be03735bd8e7625e3e
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-09 15:38:55 +0700

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,3 +31,15 @@ def private_panel(_):
         ],
     ]
     return buttons
+
+def alive_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+        ],
+    ]
+    return buttons
+
