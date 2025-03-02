cbadf9ff9bec2de4cf5591fc3bf3e284734b3746
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-03 00:10:45 +0700

    Update logging.py

--- AnonXMusic/logging.py
+++ AnonXMusic/logging.py
@@ -1,11 +1,16 @@
 import logging
+from logging.handlers import RotatingFileHandler
+
+from config import LOG_FILE_NAME
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        logging.FileHandler("log.txt"),
+        RotatingFileHandler(
+            LOG_FILE_NAME, maxBytes=5000000, backupCount=10
+        ),
         logging.StreamHandler(),
     ],
 )
@@ -17,3 +22,4 @@ logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+    
