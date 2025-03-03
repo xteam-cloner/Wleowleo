faff37be6e610cc0e76efc84d6258bf4d9615a8d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-03 13:39:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", ""))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002385138723"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", ))
